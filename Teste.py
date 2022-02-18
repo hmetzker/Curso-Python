9e0cec434e04523dfc7b08a92c7e8e221fedf896
Haroldo <hmetzker@gmail.com>
2022-02-18 15:58:42 -0300

    Cópia do Project1

---
+++ Teste.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+n1 = input("Número 1 = ")
+n2 = input("Número 2 = ")
+
+def mult_two (n1, n2):
+    return n1 * n2
+
+if __name__ == "__main__":
+    print("Exemplo:")
+    print(mult_two(n1, n2))
+
+#    assert mult_two(3, 2) == 6
+#    assert mult_two(1, 0) == 0
+#    print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ Teste2.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def first_word(text):
+    """
+    returns the first word in a given text.
+    """
+    # your code here
+
+    text = text.split()
+    return text[0]
+
+if __name__ == "__main__":
+    print("Example:")
+    print(first_word("Hello world"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word("a word") == "a"
+    assert first_word("hi") == "hi"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
