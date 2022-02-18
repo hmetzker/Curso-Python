5f2efbb37cf287c6a6ab5a879273be488c45be89
Haroldo <hmetzker@gmail.com>
2022-02-18 15:17:40 -0300

    Identação

--- Teste_gitHUB.py
+++ Teste_gitHUB.py
@@ -1,9 +1,11 @@
 # teste gitHUB
 
-n=""; m=""
+n=""
+m=""
 def result(n, m):
     return n ** m
 
 if __name__ == '__main__':
-    n=int(input(n)); m=int(input(m))
+    n=int(input(n))
+    m=int(input(m))
     print(result(n, m))
