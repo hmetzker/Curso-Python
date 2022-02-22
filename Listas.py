4f79d786f16e7ab5ad7ed535ae9d794cda0fb01e
Haroldo <hmetzker@gmail.com>
2022-02-22 20:40:04 -0300

    Trabalhando com listas

---
+++ Listas.py
@@ -0,0 +1,18 @@
+import random
+
+def generate():
+    l = list()
+    for i in range(10):
+        l.append(random.randint(0, 10))
+    return l
+
+if __name__ == '__main__':
+    a = generate()
+    print(a)
+    print(a.pop())
+    print(a.pop())
+    print(a)
+    for i in range(len(a)):
+        print(i)
+        print(a[i])
+        print(f'O i é {i} e o a[i] é {a[i]}')
