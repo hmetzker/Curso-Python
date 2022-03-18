d3c16badd7b3fb7f3718ce7e2f5fb9893e7f66c3
Haroldo <hmetzker@gmail.com>
2022-03-17 22:25:46 -0300

    Manipulação de arquivos + tarefa PyCheckIO

--- ManipulaTXT.py
+++ ManipulaTXT.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 
 def geraArq(s1, s2, a1, a2):
     try:
@@ -13,6 +15,9 @@ def geraArq(s1, s2, a1, a2):
     except:
         print("Erro geraArq: arquivo2")
 
+    f.close()
+    g.close()
+
 def addArq(s1, s2, a1, a2):
     try:
         with open(arquivo1, 'a') as f:
@@ -26,22 +31,29 @@ def addArq(s1, s2, a1, a2):
     except:
         print("Erro addArq: arquivo2")
 
+    f.close()
+    g.close()
+
 def alteraArq(s1, s2, a1, a2):
-    s3=s1.split()
-    try:
-        with open(arquivo1, 'r') as f:
-            lida = f.read()
-    except:
+    if os.path.exists(arquivo1):
+       with open(arquivo1, 'r') as f:
+           s3=f.read()
+           s3=s3.split()
+
+           try:
+               with open(arquivo2, 'a') as g:
+                   for i in range(len(s3)):
+                       g.write(f'{s3[i]}\n')
+
+                   g.write(s2)
+           except:
+               print("Erro alteraArq: arquivo2")
+    else:
         print("Erro alteraArq: arquivo1")
+        sys.exit()
 
-    try:
-        with open(arquivo2, 'a') as g:
-            for i in range(len(s3)):
-                g.write(f'{s3[i]}\n')
-
-            g.write(s2)
-    except:
-        print("Erro alteraArq: arquivo2")
+    f.close()
+    g.close()
 
 if __name__ == '__main__':
     string1='haroldo fátima bianca bruna'
